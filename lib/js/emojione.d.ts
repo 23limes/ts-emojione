0f0dd23b5e1bfe136e1ec670adb015c829a00406
Cam Jones <23limes@gmail.com>
2016-06-21 11:45:00 -0700

    adding first functions

--- lib/js/emojione.d.ts
+++ lib/js/emojione.d.ts
@@ -1,5 +1,7 @@
 declare namespace emojione {
-
+  //TODO: add other functions as needed, this is currently all I'm using
+  function toShort(str: string): string;
+  function shortnameToImage(str: string): string;
 }
 
 export = emojione;
